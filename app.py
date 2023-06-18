ce3eaf50017b06025c056c74a65e0055f7667b57
saad-codes <muhammadsaadmath.com>
2023-06-18 22:51:30 +0500

    update

--- app.py
+++ app.py
@@ -14,9 +14,14 @@ app = Flask(__name__)
 def hello_world():
 	return 'Hello World'
 
+@app.route("/login")
+def login():
+	return 'Enter Your Email'
+
+
 # main driver function
 if __name__ == '__main__':
 
 	# run() method of Flask class runs the application
 	# on the local development server.
-	app.run(host  = "0.0.0.0",port=80)
+	app.run(host  = "0.0.0.0")
